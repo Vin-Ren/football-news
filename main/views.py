892ee09a4f06dc3fd1886de9909cf744c64befb5
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-09-17 13:45:18 +0700

    feat: Tutorial03

--- main/views.py
+++ main/views.py
@@ -1,18 +1,29 @@
+import datetime
 from django.shortcuts import get_object_or_404, redirect, render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.core import serializers
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
+
 
 from main.forms import NewsForm
 from main.models import News
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    news_list = News.objects.all()
+    filters = request.GET.get('filter', 'all')
+    news_list = News.objects.all() if filters == 'all' else News.objects.filter(user=request.user)
     context = {
         'npm': '2406353225',
         'name': 'Vincent Valentino Oei',
         'class': "PBP E",
-        'news_list': news_list
+        'news_list': news_list,
+        'last_login': request.COOKIES.get('last_login')
     }
     return render(request, "main.html", context)
 
@@ -26,6 +37,7 @@ def show_json(request):
     data = serializers.serialize("json", news_list)
     return HttpResponse(data, content_type="application/json")
 
+@login_required(login_url='/login')
 def show_news(request, id):
     news = get_object_or_404(News, pk=id)
     news.increment_views()
@@ -50,11 +62,13 @@ def show_json_by_id(request, news_id):
     except News.DoesNotExist:
         return HttpResponse(status=404)
 
-def create_news(request):
+def create_news(request: HttpRequest):
     form = NewsForm(request.POST or None)
     
     if form.is_valid() and request.method == "POST":
-        form.save()
+        news = form.save(commit=False)
+        news.user = request.user
+        news.save()
         return redirect('main:show_main')
     
     ctx = {
@@ -62,3 +76,38 @@ def create_news(request):
     }
     
     return render(request, 'create_news.html', context=ctx)
+
+def register(request: HttpRequest):
+    form = UserCreationForm(request.POST or None)
+    
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        messages.success(request, 'Your account has been successfully created!')
+        return redirect('main:login')
+    ctx = {
+        'form': form
+    }
+    return render(request, 'register.html', context=ctx)
+
+def login_user(request: HttpRequest):
+    
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST or None)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse('main:show_main'))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+        form = AuthenticationForm(request)
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context=context)
+
+def logout_user(request: HttpRequest):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:show_main'))
+    response.delete_cookie('last_login')
+    return response
